a894f9d97a2b34058fe0ab05753645522dd938f0
3la2a7mad <alaa056945@gmail.com>
2025-05-11 21:23:53 +0300

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const originalnum = [1,2,3,4,5];
-const originalArrayContent = document.getElementById('originalArrayContent');
-originalArrayContent.textContent = '['+ originalnum.join(', ') + ']';
-
-const originalDnum =originalnum.map(Number => Number *2);
-const doubledArrayContent = document.getElementById('doubledArrayContent');
-doubledArrayContent.textContent = '[' + originalDnum.join(', ')+']';
+const doubleNumbers = (numbers) => {
+    const doubled = numbers.map(num => num * 2);
+    document.getElementById('originalArrayContent').textContent = '[' + numbers.join(', ') + ']';
+    document.getElementById('doubledArrayContent').textContent = '[' + doubled.join(', ') + ']';
+  };
+  
+  doubleNumbers([1, 2, 3, 4, 5]);
